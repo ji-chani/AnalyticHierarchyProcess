06644c40b6e72b4d67cfeda62791ae9d84f5f332
Cristian Jetomo <120572492+ji-chani@users.noreply.github.com>
2024-05-19 22:57:51 +0800

    Update ahp.py

--- ahp.py
+++ ahp.py
@@ -2,8 +2,6 @@
 
 import numpy as np
 import pandas as pd
-import networkx as nx
-import matplotlib.pyplot as plt
 
 class Hierarchy:
     def __init__(self) -> None:
@@ -46,14 +44,15 @@ class Hierarchy:
             self.decision_weights = np.matmul(self.decision_weights, layer.weights)
 
         # construct ranking
-        print('AHP is solved: ')
+        print()
+        print('AHP is solved --------------')
         final_result = np.array([[self.layers[-1].labels[idx], self.decision_weights[idx]] for idx in range(len(self.decision_weights))])
-        ranking = np.argsort(self.decision_weights)[::-1] + 1 # reversed or descending
-        ranking = ranking.reshape(len(ranking),1)
+        ranking = np.array([sorted(self.decision_weights, reverse=True).index(x)+1 for x in self.decision_weights])  # descending order (reverse = True)
+        ranking = ranking.reshape(len(ranking), 1)
         final_result = np.append(final_result, ranking, axis=1)
         print(pd.DataFrame(final_result, columns=[self.layers[-1].id, 'decision weights', 'rank']))
 
-        return self.decision_weights
+        return self.decision_weights, ranking.flatten()
 
 class Layer(Hierarchy):
     """
@@ -70,7 +69,7 @@ class Layer(Hierarchy):
         self.id = unique_id
 
     # update weights of layer (input_type options: matrix, values, or rank)
-    def update_weights(self, layer_idx:int=None, input_type:str=None, values:list=None, comparison_matrix:np.ndarray=None, reverse:bool=False):
+    def update_weights(self, layer_idx:int=None, input_type:str=None, values:list=None, comparison_matrix:np.ndarray=None, ranking:list=None, reverse:bool=False):
 
         if input_type == 'matrix':
             if layer_idx is None:
@@ -88,11 +87,13 @@ class Layer(Hierarchy):
                 self.weights[layer_idx] = values / np.sum(values)
 
         if input_type == 'rank':
-            pass
+            rank_matrix = rank_difference(ranking)
+            if layer_idx is None:
+                self.weights = get_weights(normalize_matrix(rank_matrix))
+            else:
+                self.weights[layer_idx] = get_weights(normalize_matrix(rank_matrix))
 
-
-# HELPER FUNCTIONS --------
-
+# -------- HELPER FUNCTIONS
 def normalize_matrix(pairwise_matrix:np.ndarray) -> np.ndarray:
     """ Computes for the corresponding normalized matrix of a given pairwise comparison matrix"""
     column_sum = np.sum(pairwise_matrix, axis=0)
@@ -115,5 +116,28 @@ def get_consistency_ratio(pairwise_matrix:np.ndarray):
         inconsistency = 'acceptable'
     return consistency_ratio, inconsistency
 
-def rank_difference():
-    pass
+def rank_difference(rankings:list):
+    
+    num_ranks = len(rankings)
+    num_weights = int(np.max(rankings))
+
+    # create matrix placeholder
+    rank_matrix = np.ones(shape=(num_ranks, num_ranks))
+
+    # create rank corresponding weights
+    unrounded_rank_weights = np.ones(num_weights)
+    if num_weights != 1:
+        unrounded_rank_weights[-1] = 9
+    for i in range(1, num_weights-1):
+        unrounded_rank_weights[i] = unrounded_rank_weights[i-1] + 8/(num_weights-1)
+    rank_weights = [round(unrounded_rank_weights[i]) for i in range(num_weights)]
+
+    # update matrix based on ranking
+    for i in range(rank_matrix.shape[0]):
+        for j in range(rank_matrix.shape[1]):
+            diff = rankings[i] - rankings[j]
+            if diff <= 0:
+                rank_matrix[i,j] = rank_weights[-int(diff)]
+            else:
+                rank_matrix[i,j] = 1/rank_weights[int(diff)]
+    return rank_matrix
